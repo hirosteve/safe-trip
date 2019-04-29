c4ff4c934d5b5149e70b392fced2fdc5a905f1f8
Steve <stephensajose@gmail.com>
2019-04-29 13:40:14 +0100

    update seatlayout

---
+++ assets/plugins/js/seatmap.js
@@ -0,0 +1,61 @@
+window.addEventListener('DOMContentLoaded', (e) => {
+  const seatContainer = document.getElementById('selected-seats-no');
+  const availableSeat = document.getElementsByClassName('available-seat');
+  const selectedSeat = document.getElementsByClassName('selected-seat');
+  const counter = document.getElementById('counter');
+  const totalPrice = document.getElementById('total-price');
+  const grandTotal = document.getElementById('grand-total');
+  const discount = document.getElementById('discount');
+
+  const renderSeat = (seatLayout) => {
+    const seatContainer = document.getElementsByClassName('seat-map-container')[0];
+    seatContainer.insertAdjacentHTML('afterbegin', seatLayout);
+  }
+
+  const addSeat = (seatNo) => {
+    const li = document.createElement('li')
+    let node = document.createTextNode(`Seat #${seatNo}, `);
+    li.id = seatNo;
+    li.appendChild(node);
+    seatContainer.appendChild(li);
+  }
+
+  const removeSeat = (seatNo) => {
+    const child = document.getElementById(seatNo);
+    seatContainer.removeChild(child);
+  }
+
+  const countSeat = () => {
+    const { length } = selectedSeat;
+    counter.innerHTML = `x ${length}`;
+    return length;
+  }
+
+  const calculatePrice = (price) => {
+    const value = countSeat();
+    const total = price * value;
+    totalPrice.innerHTML = `${total}.00`;
+    return total;
+  }
+
+  calculateGrandTotal = (price, priceDiscount = 0) => {
+    const totalPrice = calculatePrice(price);
+    const grand = totalPrice - priceDiscount;
+    discount.innerHTML = `${priceDiscount}.00`;
+    grandTotal.innerHTML = `${grand}.00`;
+  }
+
+  for (let i = 0; i < availableSeat.length; i++) {
+        availableSeat[i].addEventListener('click', () => {
+        availableSeat[i].classList.toggle('selected-seat');
+        const seatNo = (availableSeat[i].children[1].innerText);
+        if (availableSeat[i].classList.contains('selected-seat')) {
+          addSeat(seatNo); 
+        }
+        else {
+          removeSeat(seatNo);
+        }
+        calculateGrandTotal(1000, 500);
+    })
+  }
+});
